bfdbe2c88edd79df4838dcdbe9a0ae21ef926219
Terra-flu <putdao@tutamail.com>
2026-03-15 01:43:26 +0500

    Update bot.py
    добавил вначале,
    import logging
    logger = logging.getLogger(__name__)
    хотя ошибка на 518

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import logging
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 import json
 from datetime import datetime
 
