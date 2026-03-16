0f2fd446a0459390a37d65ee75a9b7dbfe41701b
Terra-flu <putdao@tutamail.com>
2026-03-16 20:06:19 +0500

    Update bot.py
    пробел на 551

--- bot.py
+++ bot.py
@@ -547,7 +547,6 @@ for i, entry in enumerate(cart_items):
         [InlineKeyboardButton(text="✅ Оформить заказ", callback_data="checkout")],
         [InlineKeyboardButton(text="🗑️ Очистить корзину", callback_data="clear_cart")]
     ]
-
     await message.answer(caption, reply_markup=InlineKeyboardMarkup(inline_keyboard=kb))
 
 # 🛒 Показ корзины (через кнопку "🛒 Корзина" в меню)
